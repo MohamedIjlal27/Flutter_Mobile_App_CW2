8e4061cc54ecb6db2a13a0846347e5502b7389ef
MohamedIjlal27 <mohamedijlal27@gmail.com>
2025-01-07 12:39:55 +0530

    Logo Attached

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const appname = 'E-Travel';
+const appname = 'ETravel';
 const splashtagline = 'Explore the World with Ease';
 var applogo = Image.asset(
   'assets/images/logo.png',
